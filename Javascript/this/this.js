2866d170007f2a98ba23c94943a004649d2f5621
Atul8766 <atul61541@gmail.com>
2025-05-31 00:30:07 +0530

    tricky conditions

---
+++ Javascript/this/this.js
@@ -0,0 +1,35 @@
+const person = {
+  name: "ram",
+  age: 22,
+  greet: function () {
+    return `Hello , you are ${this.age} years old`;
+  },
+  //   greet: () => {
+  //     return `Hello , you are ${this.age} years old`;
+  //   },
+};
+console.log(person.greet());
+
+const person = {
+  name: "GeeksforGeeks",
+  greet() {
+    return `Welcome To, ${this.name}`;
+  },
+};
+console.log(person.greet());
+function greet() {
+  console.log("Hello, my name is " + this.name);
+}
+
+const person1 = {
+  name: "Amit",
+  sayHello: greet,
+};
+const anotherPerson = {
+  name: "Jatin",
+};
+
+//Driver Code Starts
+greet();
+person.sayHello();
+greet.call(anotherPerson);
